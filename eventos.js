e1ad7a13d61eb4dcfe27d5e7ea1ff5fe65415b88
joaquinMoreiraF <ferreiramoreirajoaquin@gmail.com>
2023-09-03 23:00:20 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
 const divElement = document.createElement("div");
 divElement.style.padding = "5px";
 divElement.style.backgroundColor = "green";
-divElement.textContent = "Haz clic en mí";
+
 
 document.body.appendChild(divElement);
 
