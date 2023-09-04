81d72459a370073f361f9455c7ea44584ddb19cf
joaquinMoreiraF <ferreiramoreirajoaquin@gmail.com>
2023-09-03 23:13:48 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,6 +1,8 @@
 const divElement = document.createElement("div");
 divElement.style.padding = "5px";
 divElement.style.backgroundColor = "green";
+divElement.style.height = "50px";
+divElement.textContent = "Click aqui";
 
 
 document.body.appendChild(divElement);
